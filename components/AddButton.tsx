9e4586e2e679cea35ee7083333a6c096b2cda150
Ajarek <ajarek@poczta.onet.pl>
2024-09-21 00:34:48 +0200

    added component addButton

---
+++ components/AddButton.tsx
@@ -0,0 +1,49 @@
+import { Button } from './ui/button'
+
+import React from 'react'
+type Array= {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+
+}
+
+type Props={
+  onClick:React.MouseEventHandler<HTMLButtonElement>
+  array:Array[]
+}
+
+ const AddButton = ({ onClick, array }:Props )=> {
+  return (
+    <div className='flex flex-wrap items-center justify-center gap-4 p-8'>
+      {array
+        .filter((cr, inx, arr) => inx < arr.length / 10)
+        .map((btn, inx) => (
+          <Button
+            key={inx}
+            onClick={onClick}
+            className='focus:bg-primary/60'
+          >
+            {inx + 1}
+          </Button>
+        ))}
+    </div>
+  )
+ }
+
+export default AddButton
+ 
--- components/CryptocurrencyTable.tsx
+++ components/CryptocurrencyTable.tsx
@@ -10,54 +10,39 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { Button } from './ui/button'
-
 import React from 'react'
-
-export const AddButton = ({onClick}: any) => {
-  return (
-    <div className='flex items-center justify-center gap-4 p-8'>
-      {crypto
-      .filter((cr,inx,arr)=>inx<arr.length/10)
-      .map((btn,inx) => (
-          <Button
-        key={inx}
-        onClick={onClick}
-        className=''
-      >
-        {inx+1}
-      </Button>
-        )
-      )}
-      </div>
-  )
-}
-
+import AddButton from '@/components/AddButton'
 
 const CryptocurrencyTable = () => {
-  const [end, setEnd]=React.useState(5)
-  const handleButton=(e)=>{
-    setEnd((e.target.innerText)*5)
+  const [end, setEnd] = React.useState(10)
+  const handleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLInputElement
+    setEnd(+target.innerText * 10)
   }
   return (
     <div className='container p-4'>
-      <h1 className='text-xl font-semibold'>Cryptocurrency list:</h1>
+      <h1 className='text-xl font-semibold'>Cryptocurrency list length: {crypto.length}</h1>
       <Table>
-        <TableCaption><AddButton onClick={handleButton}/></TableCaption>
+        <TableCaption>
+          <AddButton
+            onClick={handleButton}
+            array={crypto}
+          />
+        </TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className=''>Logo</TableHead>
             <TableHead className=''>Name</TableHead>
             <TableHead className=''>Price</TableHead>
             <TableHead className=''>Price change 24h</TableHead>
-            <TableHead className=''>Price change % 24h</TableHead>
-            <TableHead className=''>Total volume</TableHead>
+            <TableHead className='max-sm:hidden'>Price change % 24h</TableHead>
+            <TableHead className='max-sm:hidden'>Total volume</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {crypto
             .sort((a, b) => b.current_price - a.current_price)
-            .filter((cr, idx) => idx < end && idx>=end-5)
+            .filter((cr, idx) => idx < end && idx >= end - 10)
             .map((cr) => (
               <TableRow key={cr.id}>
                 <TableCell className=''>
@@ -79,17 +64,20 @@ const CryptocurrencyTable = () => {
                 >
                   ${cr.price_change_24h.toFixed(2)}
                 </TableCell>
-                <TableCell  className={
-                    cr.price_change_percentage_24h > 0 ? 'text-green-500' : 'text-red-500'
-                  }>
+                <TableCell
+                  className={
+                    cr.price_change_percentage_24h > 0
+                      ? 'text-green-500 max-sm:hidden'
+                      : 'text-red-500 max-sm:hidden'
+                  }
+                >
                   {cr.price_change_percentage_24h}%
                 </TableCell>
-                <TableCell className=''>{cr.total_volume}</TableCell>
+                <TableCell className='max-sm:hidden'>{cr.total_volume}</TableCell>
               </TableRow>
             ))}
         </TableBody>
       </Table>
-      
     </div>
   )
 }
