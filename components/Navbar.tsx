8757bd59f926fed9cf664d1220dd6e0731e80344
Ajarek <ajarek@poczta.onet.pl>
2024-09-26 23:55:17 +0200

    added meta robots

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import { auth } from '@/app/api/auth/auth'
 import Logout from './Logout'
 const Navbar = async () => {
   const session = await auth()
-  console.log(session)
+  
   return (
     <div className='h-16  w-full  flex justify-between items-center gap-4  px-16 max-sm:px-2  '>
       <div className='w-full flex items-center gap-2 '>
